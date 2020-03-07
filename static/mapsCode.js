640b0f77549edb59483ef23c70baa92e8729ad56
plessard <lessardpw@gmail.com>
2020-03-07 17:08:34 -0500

    commit

--- static/mapsCode.js
+++ static/mapsCode.js
@@ -24,7 +24,7 @@ function initMap() {
     var uluru = {lat: latitude, long_c: longitude};
 
     var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
+      document.getElementById('map'), {zoom: 8, center: uluru});
 
     var marker = new google.maps.Marker({position: uluru, map: map});
   
