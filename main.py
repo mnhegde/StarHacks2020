d071b1ee3bb5d5acb23669744604913c483b85f1
Manu Hegde <manu.hegde02@gmail.com>
2020-03-07 11:30:53 -0500

    added countries. finalized layout

--- main.py
+++ main.py
@@ -77,12 +77,12 @@ def signup():
 
 @app.route('/',  methods = ['GET','POST'])
 def index():
-    return render_template('layout2.html')
-
-
-@app.route('/home',  methods = ['GET','POST'])
-def home():
     return render_template('home.html')
+
+
+@app.route('/about',  methods = ['GET','POST'])
+def home():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
