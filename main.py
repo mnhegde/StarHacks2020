7c8ccc58aa614f8d1b2098b1cf3d2d7d629c583d
plessard <lessardpw@gmail.com>
2020-03-07 09:13:22 -0500

    LAYOUT to Login, Home page

--- main.py
+++ main.py
@@ -75,5 +75,9 @@ def signup():
     else:
         return render_template('signup.html')
 
+@app.route('/',methods=["GET","POST"])
+def home():
+    return render_template('layout.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
