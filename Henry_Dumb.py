b8cdc21608a15c3ca461f0f6530edfedd691f788
Manu Hegde <manu.hegde02@gmail.com>
2020-03-06 21:51:42 -0500

    user authorization (login and signup system) with password hashing. Will include database later

--- Henry_Dumb.py
+++
@@ -1,5 +0,0 @@
-coded_message = ["henry",'dumb','amiright?']
-
-for r in coded_message:
-    print(r)
-print('and he loves balls')
---
+++ main.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, session, request, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+import json
+from hashpassword import makePasswordHash, checkPasswordHash
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'hello'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///UserLoginInfoDB.sqlite3'
+
+
+#user_db = SQLAlchemy(app)
+
+#class User(db.Model):
+    #id = db.Column(db.Integer, primary_key=True)
+    #firstname = db.Column(db.String(50))
+    #lastname = db.Column(db.String(50))
+    #username = db.Column(db.String(50))
+    #password = db.Column(db.String(50))
+    #date_created = db.Column(db.DateTime, default=datetime.now)
+
+
+users = [['bobsmith', makePasswordHash('hello'), 'bob', 'smith', '123@gmail.com'], ['henrysmith', makePasswordHash('world'), 'henry', 'smith', '123@gmail.com'], ['thomaswayne', makePasswordHash('space'), 'thomas', 'wayne', '123@gmail.com'], ['timcook', makePasswordHash('apple'), 'tim', 'cook', '123@gmail.com']]
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        data = request.form
+        username = data['username']
+        password = data['password']
+        flag = False
+        for user in users:
+            if user[0] == username:
+                flag = True
+                if checkPasswordHash(password, user[1]):
+                    return 'logged in'
+                else:
+                    flash('The user doesn\'t exist or the password provided was incorrect', 'error')
+                    return redirect('/login')
+
+        if not flag:
+            flash('The user doesn\'t exist or the password provided was incorrect', 'error')
+            return redirect('/login')  
+
+        return 'good'
+    else:
+        return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        data = request.form
+        username = request.form['username']
+        email = request.form['email']
+        firstname = request.form['firstName']
+        lastname = request.form['lastName']
+        password = request.form['password']
+        confirmPassword = request.form['confirmPassword']
+
+        if password == confirmPassword:
+            for user in users:
+                if user[0]==username:
+                    flash('This username already exists', 'error')
+                    return redirect('/signup')
+            password = makePasswordHash(confirmPassword)
+            data = [username, password, firstname, lastname, email]
+            users.append(data)
+            return 'signed up and logged in'
+        else:
+            flash('Passwords didn\'t match!', 'error')
+            return redirect('/signup')  
+    else:
+        return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
